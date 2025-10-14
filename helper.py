0fed65094c1b32ea0534069053635014ae80a007
NAVEEN-REDDY-MADALA <166785190+NAVEEN-REDDY-MADALA@users.noreply.github.com>
2025-10-14 11:03:13 +0530

    Update helper.py

--- helper.py
+++ helper.py
@@ -7,6 +7,9 @@ from nltk.corpus import stopwords
 import emoji
 import streamlit as st
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+import nltk
+nltk.download('stopwords')
+
 analyzer =SentimentIntensityAnalyzer()
 
 
@@ -143,6 +146,7 @@ def activity_heatmap(selected_user,df):
         df = df[df["user"] == selected_user]
     user_heatmap=df.pivot_table(index="day_name", columns="period", values="message", aggfunc="count").fillna(0)
     return user_heatmap
+
 
 
 
